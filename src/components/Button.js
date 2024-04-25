67279336454452d8f080c0891b237c3f46efa137
Ana-lu1sa <ana.lima810@etec.sp.gov.br>
2024-04-25 10:38:54 -0300

    atualização Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,5 +11,28 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888888',
+    },
+    operationButton: {
+        color: '#ffffff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
+
+export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+
+    return (
+        <TouchableHighlight onPress={props.Onclick}>
+            <text style={stylesButton}>{props.label}</text>
+        </TouchableHighlight>
+    )
+}
