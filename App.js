56ddc3247f293aa2341c089a38e895e7d57952d8
Ana-lu1sa <ana.lima810@etec.sp.gov.br>
2024-05-16 08:32:04 -0300

    atualização 16/05

--- App.js
+++ App.js
@@ -35,30 +35,52 @@ export default function App(){
   }
 
 const clearMemory = () =>{
-  
+  setState(inicialState);
+}
+
+const setOperation = operation => {
+  if (state.current === 0){ 
+    setState({...state, operation, current:1, clearDisplay:true})
+  } else{
+    const equals = operation === '=';
+    const values = [...state.values];
+    try{
+      values[0] = eval(` ${values[0]} ${state.operation} ${values[1]} `);
+    } catch (e) {
+      values[0] = state.values[0];
+    }
+
+    values[1] = 0;
+    setState({
+      displayValue: `${values[0]}`,
+      operation: equals ? 0 : 1,
+      clearDisplay: !equals,
+      values,
+    });
+  }
 }
   }
   return (
-    <View style={ StyleSheet.container}>
-      <Display value={this.state.DisplayValue} />
-      <View style={StyleSheet.Button}>
+    <View style={ styles.container}>
+      <Display value={state.DisplayValue} />
+      <View style={styles.Button}>
       <button label='AC' triple onClick={clearMemory}/>
-      <button label='/'  operation onClick={this.setOperation}/>
+      <button label='/'  operation onClick={setOperation}/>
       <button label='7'  onClick={addDigit}/>
       <button label='8'  onClick={addDigit}/>
       <button label='9'  onClick={addDigit}/>
-      <button label='*'  operation onClick={this.setOperation}/>
+      <button label='*'  operation onClick={setOperation}/>
       <button label='4'  onClick={addDigit}/>
       <button label='5'  onClick={addDigit}/>
       <button label='6'  onClick={addDigit}/>
-      <button label='-'  operation onClick={this.setOperation}/>
+      <button label='-'  operation onClick={setOperation}/>
       <button label='1'  onClick={addDigit}/>
       <button label='2'  onClick={addDigit}/>
       <button label='3'  onClick={addDigit}/>
-      <button label='+'  operation onClickk={this.setOperation}/>
+      <button label='+'  operation onClickk={setOperation}/>
       <button label='0'  double onClick={addDigit}/>
-      <button label='.'  operation onClick={this.setOperation}/>
-      <button label='='  operation onClick={this.setOperation}/>
+      <button label='.'  operation onClick={setOperation}/>
+      <button label='='  operation onClick={setOperation}/>
 
       </View>
       </View>
